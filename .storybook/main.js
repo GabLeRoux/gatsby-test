822ce1ab284c94b0dcf6a7fbfa134671f1857152
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2023-10-25 00:41:34 -0400

    Update storybook configuration and preview settings

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,52 @@
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+const { resolve } = require('path');
+const sassConfig = require('../sass-loader.config');
+module.exports = {
+  stories: ['../@(src|stories)/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        sassLoaderOptions: sassConfig,
+      },
+    },
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@storybook/addon-mdx-gfm',
+    '@storybook/addon-storysource',
+    '@storybook/addon-a11y',
   ],
+  webpackFinal: async (config, { configType }) => {
+    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    config.module.rules.push({
+      test: /\.js$/,
+      exclude: /node_modules\/(?!(gatsby)\/)/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: [
+              require.resolve('babel-plugin-remove-graphql-queries'),
+              // any other plugins you need to use
+            ],
+          },
+        },
+      ],
+    });
+    // Add Sass/SCSS support
+    config.module.rules.push({
+      test: /\.(s*)css$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: resolve(__dirname, '../'),
+    });
+
+    return config;
+  },
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: true,
   },
 };
-export default config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,7 @@
 /** @type { import('@storybook/react').Preview } */
+
+import '@/scss/main.scss';
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -7,6 +10,16 @@ const preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      default: 'Light',
+      values: [
+        { name: 'Light', value: '#fff' },
+        // Same as our brand #161843e6 or rgba(22, 24, 67, .9) on white
+        { name: 'Background', value: '#161843' },
+        { name: 'Dark', value: '#101522' },
+        { name: 'Darkest', value: '#040919' },
+      ],
     },
   },
 };
