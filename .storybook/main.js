ecf2bc6ed51b92e0cf339df1d724c13afdf11cf6
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2023-10-24 23:57:49 -0400

    Recreated storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,44 +1,19 @@
-const { resolve } = require('path');
-module.exports = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@storybook/addon-notes/register',
+    '@storybook/addon-mdx-gfm',
   ],
-  webpackFinal: async (config, { configType }) => {
-    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
-    config.module.rules.push({
-      test: /\.js$/,
-      exclude: /node_modules\/(?!(gatsby)\/)/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: [
-              require.resolve('babel-plugin-remove-graphql-queries'),
-              // any other plugins you need to use
-            ],
-          },
-        },
-      ],
-    });
-    // Add Sass/SCSS support
-    config.module.rules.push({
-      test: /\.(s*)css$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: resolve(__dirname, '../'),
-    });
-
-    return config;
-  },
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
   },
   docs: {
-    autodocs: true,
+    autodocs: 'tag',
   },
 };
+export default config;
